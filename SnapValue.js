7d35bd38dbd2e2d752c82913f9251986a3b4a07c
Jonas Hjalmarsson <jonas@byjalma.se>
2023-12-06 09:08:08 +0100

    SnapValue script added

---
+++ SnapValue.js
@@ -0,0 +1,160 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+define(["require", "exports", "system_lib/Metadata", "system_lib/Script", "system/SimpleFile"], function (require, exports, Metadata_1, Script_1, SimpleFile_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var SnapValue = (function (_super) {
+        __extends(SnapValue, _super);
+        function SnapValue(env) {
+            var _this = _super.call(this, env) || this;
+            _this.readJsonFiles();
+            return _this;
+        }
+        SnapValue.prototype.reload = function () {
+            this.readJsonFiles();
+        };
+        SnapValue.prototype.readJsonFiles = function () {
+            var _this = this;
+            SimpleFile_1.SimpleFile.read(SnapValue.kFileName).then(function (data) {
+                var _a;
+                try {
+                    _this.data = JSON.parse(data);
+                    _this.mPersistent = (_a = _this.data['persistent']) !== null && _a !== void 0 ? _a : false;
+                }
+                catch (parseError) {
+                    console.error("Failed parsing JSON data from init file", SnapValue.kFileName, parseError);
+                }
+            }).catch(function (error) {
+                console.error("Failed reading init file; use initial sample data", SnapValue.kFileName, error);
+                _this.data = {
+                    "persistent": false,
+                    "value1": {
+                        'initValue': 25,
+                        'snapSize': 10,
+                        'snapValues': [
+                            500, 1670, 1850
+                        ],
+                        "precision": 0,
+                    }
+                };
+                _this.mPersistent = false;
+            }).finally(function () {
+                _this.publishProperties();
+                if (_this.mPersistent) {
+                    SimpleFile_1.SimpleFile.read(SnapValue.kPersistentFileName).then(function (data) {
+                        try {
+                            _this.values = JSON.parse(data);
+                        }
+                        catch (parseError) {
+                            console.error("Failed parsing JSON data from persistent file", SnapValue.kPersistentFileName, parseError);
+                            _this.initValues();
+                        }
+                    }).catch(function (error) {
+                        console.error("Failed reading persistent file; use initial values", SnapValue.kPersistentFileName, error);
+                        _this.initValues();
+                    });
+                }
+                else {
+                    _this.initValues();
+                }
+            });
+        };
+        SnapValue.prototype.initValues = function () {
+            this.values = new Array();
+            for (var key in this.data) {
+                if (key == "persistent")
+                    continue;
+                this.values[key] = this.data[key]["initValue"];
+            }
+            this.persistVars();
+        };
+        SnapValue.prototype.publishProperties = function () {
+            for (var key in this.data) {
+                var propData = this.data[key];
+                var typeName = typeof propData;
+                if (key == "persistent")
+                    continue;
+                if (typeName === 'object') {
+                    this.makeProperties(key);
+                }
+                else
+                    console.error("Invalid type of ", key, typeName);
+            }
+        };
+        SnapValue.prototype.makeProperties = function (name) {
+            var _this = this;
+            try {
+                this.property(name, { type: Number }, function (value) {
+                    var _a;
+                    if (value !== undefined && value !== _this.data[name]) {
+                        var precision = (_a = _this.data[name]['precision']) !== null && _a !== void 0 ? _a : 0;
+                        _this.values[name] = Number(value).toFixed(precision);
+                        _this.changed(name + 'Snap');
+                        _this.persistVars();
+                    }
+                    return _this.values[name];
+                });
+                this.property(name + "Snap", { type: Number, readOnly: true }, function () {
+                    var _a;
+                    var currValue = _this.values[name];
+                    var size = _this.data[name]['snapSize'];
+                    var values = _this.data[name]['snapValues'];
+                    var precision = (_a = _this.data[name]['precision']) !== null && _a !== void 0 ? _a : 0;
+                    for (var i = 0; i < values.length; i++) {
+                        var val = Number(values[i]);
+                        console.log('val: ' + val + ' value: ' + currValue);
+                        if (val - size < currValue && val + size > currValue)
+                            return val.toFixed(precision);
+                    }
+                    return Number(_this.values[name]).toFixed(precision);
+                });
+            }
+            catch (error) {
+                console.warn("Already initialized property " + name);
+            }
+        };
+        SnapValue.prototype.persistVars = function () {
+            var _this = this;
+            if (!this.mPersistent)
+                return;
+            if (!this.mPersistor) {
+                this.mPersistor = wait(200);
+                this.mPersistor.then(function () {
+                    delete _this.mPersistor;
+                    var jsonData = JSON.stringify(_this.values, null, 2);
+                    SimpleFile_1.SimpleFile.write(SnapValue.kPersistentFileName, jsonData);
+                });
+            }
+        };
+        SnapValue.kFileName = "SnapValue.json";
+        SnapValue.kPersistentFileName = "SnapValuePersistent.json";
+        __decorate([
+            Metadata_1.callable("Reload settings from JSON file"),
+            __metadata("design:type", Function),
+            __metadata("design:paramtypes", []),
+            __metadata("design:returntype", void 0)
+        ], SnapValue.prototype, "reload", null);
+        return SnapValue;
+    }(Script_1.Script));
+    exports.SnapValue = SnapValue;
+});
---
+++ SnapValue.ts
@@ -0,0 +1,183 @@
+/*
+	A Snap Value functionality to snap values to a set of fixed values with a snapSize. 
+	Sample:
+		"persistent": false, // if persistent is enabled, the values will be saved to a file and loaded on startup.
+		"value1": {
+			'initValue': 25, // the initial value of the variable
+			'snapSize': 10, // the snapsize of the variable
+			'snapValues': [ // the values to snap to
+				500, 1670, 1850
+			],
+			"precision": 0, // the precision of the variable
+		}
+	
+	The script publishes each value as a property with the name of the value and one with the name of the value + "Snap"
+	which contains the snapped value. The script also publishes a reload function to reload the values from the json file.
+
+	If persistent is enabled, the values will be saved to a file and loaded on startup.
+
+	The script is based on PixiLab's Persistant.ts script.
+
+	Script made by Jonas Hjalmarsson, By Jalma AB, Sweden (https://byjalma.se).
+	Idea and ordered by Petter Feltenstedt, Kalmar Mediespecialist AB, Sweden (http://mediespecialist.se).
+	All Rights Reserved.
+*/
+import { callable } from "system_lib/Metadata";
+import { Script, ScriptEnv } from "system_lib/Script";
+import { SimpleFile } from "system/SimpleFile";
+import { PrimTypeSpecifier } from "system/PubSub";
+
+export class SnapValue extends Script {
+	private data: any;
+	private values: any;
+	private mPersistor: CancelablePromise<any>;	// Timer when persistent write pending
+	private mPersistent: false;
+	private static kFileName = "SnapValue.json";	// Name of file where I persist my data
+	private static kPersistentFileName = "SnapValuePersistent.json";	// Name of file where I persist my data
+
+	public constructor(env: ScriptEnv) {
+		super(env);
+		this.readJsonFiles();
+	}
+
+	@callable("Reload settings from JSON file")
+	public reload() {
+		this.readJsonFiles()
+	}
+
+	/**
+	 * Read the JSON readJsonFiles
+	 */
+	private readJsonFiles() {
+		SimpleFile.read(SnapValue.kFileName).then(data => {
+			try {
+				this.data = JSON.parse(data)
+				this.mPersistent = this.data['persistent'] ?? false
+			} catch (parseError) {
+				console.error("Failed parsing JSON data from init file", SnapValue.kFileName, parseError);
+			}
+		}).catch(	// Failed reading file.
+			error => {
+				console.error("Failed reading init file; use initial sample data", SnapValue.kFileName, error);
+				//  Likely had no file. Init with some sample data
+				this.data = {
+					"persistent": false,
+					"value1": {
+						'initValue': 25,
+						'snapSize': 10,
+						'snapValues': [
+							500, 1670, 1850
+						],
+						"precision": 0,
+					}
+				}
+				this.mPersistent = false
+			}
+		).finally(() => {
+			this.publishProperties() // Publish the actual properties
+
+			if (this.mPersistent) { // read last values if persistent variables enabled
+				SimpleFile.read(SnapValue.kPersistentFileName).then(data => {
+					try {
+						this.values = JSON.parse(data)
+					} catch (parseError) {
+						console.error("Failed parsing JSON data from persistent file", SnapValue.kPersistentFileName, parseError);
+						this.initValues() // init values if no persistant file is found
+					}
+				}).catch(
+					error => {
+						console.error("Failed reading persistent file; use initial values", SnapValue.kPersistentFileName, error);
+						this.initValues() // init values if no persistant file is found
+					}
+				)
+			}
+			else { // if no persistant values, init values from initValue in json file
+				this.initValues()
+			}
+
+		});
+
+	}
+
+	/**
+	 * Init default values from initValues
+	 */
+	private initValues() {
+		this.values = new Array()
+		for (const key in this.data) {
+			if (key == "persistent") continue
+			this.values[key] = this.data[key]["initValue"]
+		}
+		this.persistVars();
+
+	}
+	/**
+	 * Data was just loaded. Publish all primitive items as named properties.
+	 */
+	private publishProperties() {
+		for (const key in this.data) {
+			const propData = this.data[key];
+			// Obtain formal type from value type
+			const typeName: string = typeof propData;
+			if (key == "persistent") continue;
+			if (typeName === 'object') {
+				this.makeProperties(key);
+			}
+			else
+				console.error("Invalid type of ", key, typeName)
+		}
+	}
+
+
+	/**
+	 * Make and publish a property with name of typeName.
+	 */
+	private makeProperties(name: string) {
+		try {
+			this.property<string>(name, { type: <PrimTypeSpecifier>Number }, value => {
+				if (value !== undefined && value !== this.data[name]) {
+					// Specific and changed value - set it
+					let precision = this.data[name]['precision'] ?? 0
+					this.values[name] = Number(value).toFixed(precision);
+					this.changed(name + 'Snap')
+					this.persistVars();
+				}
+				return this.values[name];	// Always return current value
+			});
+			this.property<string>(name + "Snap", { type: <PrimTypeSpecifier>Number, readOnly: true }, () => {
+				let currValue = this.values[name]
+				let size = this.data[name]['snapSize']
+				let values = this.data[name]['snapValues']
+				let precision = this.data[name]['precision'] ?? 0
+				for (let i = 0; i < values.length; i++) {
+					let val = Number(values[i])
+					console.log('val: ' + val + ' value: ' + currValue)
+					if (val - size < currValue && val + size > currValue)
+						return val.toFixed(precision)
+
+				}
+				return Number(this.values[name]).toFixed(precision);	// Return snap-value
+			});
+		}
+		catch (error) {
+			console.warn("Already initialized property " + name)
+		}
+	}
+
+
+	/**
+	 * Make sure my persistent data is saved soon.
+	 */
+	private persistVars() {
+		if (!this.mPersistent) return
+
+		if (!this.mPersistor) {
+			this.mPersistor = wait(200);
+			this.mPersistor.then(() => {
+				delete this.mPersistor;
+				const jsonData = JSON.stringify(this.values, null, 2);
+				SimpleFile.write(SnapValue.kPersistentFileName, jsonData);
+			});
+		}
+	}
+}
